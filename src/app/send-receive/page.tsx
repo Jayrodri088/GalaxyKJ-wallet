52b1f5f148a8dc7311b575618ff6ea223a2f19c8
Josue19-08 <josuemarin2009@hotmail.com>
2025-03-14 10:47:29 -0600

    feat: add send-receive page structure

---
+++ src/app/send-receive/page.tsx
@@ -0,0 +1,9 @@
+import { SendReceiveScreen } from "@/components/send-receive-screen"
+
+export default function SendReceivePage() {
+  return (
+    <main className="min-h-screen">
+      <SendReceiveScreen />
+    </main>
+  )
+}

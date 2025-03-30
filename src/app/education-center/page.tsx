c1c486b9062a075952c866ad09bf3aa8c8db2c33
chiscookeke11 <chiscookeke11@gmail.com>
2025-03-30 21:50:06 +0100

    fixes

--- src/app/education-center/page.tsx
+++ src/app/education-center/page.tsx
@@ -3,9 +3,18 @@
 import SecurityResources from "@/components/security-resources";
 import { useState } from "react";
 import { motion } from "framer-motion";
+import { Book, BookOpen, CircleHelp, NotepadText, Shield, Trash } from "lucide-react";
 
 export default function Page() {
   const [activeTab, setActiveTab] = useState("blockchain guides");
+
+  const tabs = [
+    { icon: <Book size={20} />, label: "blockchain guides" },
+    { icon: <NotepadText size={20} />, label: "wallet tutorials" },
+    { icon: <BookOpen size={20} />, label: "crypto glossary" },
+    { icon: <CircleHelp size={20} />, label: "Advanced FAQ" },
+    { icon: <Shield size={20} />, label: "Security Resources" },
+  ];
 
   const renderTabContent = () => {
     switch (activeTab) {
@@ -16,7 +25,7 @@ export default function Page() {
       case "crypto glossary":
         return "crypto glossary content";
       case "Advanced FAQ":
-        return "Advanced FAQ";
+        return "Advanced FAQ content";
       case "Security Resources":
         return <SecurityResources />;
       default:
@@ -27,23 +36,24 @@ export default function Page() {
   return (
     <div className="w-full min-h-screen flex flex-col items-start justify-start px-5 py-6 gap-5">
       <div className="w-full flex items-center justify-between gap-6">
-        <h1>Education Center</h1>
-        <input type="search" placeholder="Search resources" />
+        <h1 className="text-3xl font-bold text-[#fffff]  "   >Education Center</h1>
+        <input type="search" placeholder="Search resources" className="border-[1px] border-[#ffffff/10] px-5 py-2  rounded-xs  " />
       </div>
 
       <div className="w-full h-full flex flex-col gap-5 items-start">
-        <nav className="w-full flex items-center justify-evenly gap-6 bg-[#13182A] backdrop-blur-md border-[1px] border-[#374151] rounded-lg px-4 py-2 overflow-x-auto">
-          {["blockchain guides", "wallet tutorials", "crypto glossary", "Advanced FAQ", "Security Resources"].map((tab) => (
+        <nav className="w-full flex items-center justify-evenly gap-6 bg-[#13182A] backdrop-blur-md border-[1px] border-[#374151] rounded-lg px-4 py-2 text-xs md:text-sm overflow-x-auto">
+          {tabs.map((tab, index) => (
             <motion.button
-              key={tab}
-              onClick={() => setActiveTab(tab)}
+              key={index}
+              onClick={() => setActiveTab(tab.label)}
               whileHover={{ scale: 1.1, backgroundColor: "#45357a" }}
               whileTap={{ scale: 0.95 }}
-              className={`cursor-pointer py-2 px-4 rounded-sm transition-all duration-300 ${
-                activeTab === tab ? "bg-[#371C5B]" : ""
+              className={`cursor-pointer flex items-center gap-2 py-2 px-4 rounded-sm transition-all duration-300 whitespace-nowrap text-[#ffffff]  ${
+                activeTab === tab.label ? "bg-[#371C5B]" : ""
               }`}
             >
-              {tab.charAt(0).toUpperCase() + tab.slice(1)}
+              {tab.icon}
+              {tab.label}
             </motion.button>
           ))}
         </nav>

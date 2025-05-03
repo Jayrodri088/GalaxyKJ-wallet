2bd876bb5893336904d46c50f154525cd57653d4
Michelle031 <mbaangel2803@gmail.com>
2025-05-03 22:47:59 +0100

    Create custom-view-tab Component for Managing Dashboard Views #31

---
+++ src/app/widget-configuration/page.tsx
@@ -0,0 +1,9 @@
+import CustomViewTab from "@/components/CustomViewTab";
+
+export default function WidgetConfigurationPage() {
+  return (
+    <main className="min-h-screen">
+      <CustomViewTab />
+    </main>
+  );
+}

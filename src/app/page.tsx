6880c5da0a9af37941338e23d4793561e8179006
Josue19-08 <josuemarin2009@hotmail.com>
2025-05-03 19:26:31 -0600

    fix: fixin main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-
-  //import { PriceAlertsTab } from "@/components/widgets"
-import VerifySignatureTab from "@/components/signature-tools/verify-signature-tab"
-// import { WelcomeScreen } from "@/components/welcome/index"
+import { WelcomeScreen } from "@/components/welcome/index"
 
 export default function HomePage() {
   return (
     <main className="min-h-screen">
- {/* <WelcomeScreen /> 
-   <PriceAlertsTab/>  */}
-      
-      <VerifySignatureTab/>
+      <WelcomeScreen />
     </main>
   )
 }
-
